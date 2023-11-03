514ff02597d03b25c88b21b466de9a520a7da2ae
adam-bahri <adambahri27@gmail.com>
2023-11-03 21:29:13 +0100

    Added comments for a better understanding of the logic

--- files/ParseAlteryxWorflow.py
+++ files/ParseAlteryxWorflow.py
@@ -42,28 +42,31 @@ root = tree.getroot()
 # Iterate over Node elements
 nodes_list = [NodeElement(x).data for x in root.iter('Node')]
 
+# Store nodes_list keys to use as headers for the csv output of nodes_list
 nodes_keys = nodes_list[0].keys()
 
 
 # Iterate over Connection elements
 connections_list = [ConnectionElement(x, nodes = nodes_list).data for x in root.iter('Connection')]
 
+#Store connections_list keys to use as headers for the csv output of connections_list
 connections_keys = connections_list[0].keys()
 
-print("Nodes_list", nodes_list)
+# print("Nodes_list", nodes_list)
 # print("Conns_list", connections_list)
 
-# Create a directed graph
+# Create a directed graph to represent the workflow
 G = nx.DiGraph()
 
-# Add nodes to the graph with position from nodes_df
+# Add nodes to the graph for each tool in nodes_list
 for node in nodes_list:
-    node_id = node['Tool ID']
-    pos_x = node['x']
-    pos_y = node['y']
-    G.add_node(node_id, pos=(pos_x, pos_y))
+    if node.get("Tool") != "ToolContainer":
+        node_id = node['Tool ID']
+        pos_x = node['x']
+        pos_y = node['y']
+        G.add_node(node_id, pos=(pos_x, pos_y))
 
-# Add edges to the graph based on OriginToolID and DestinationToolID from connections_df
+# Add edges to the graph based on OriginToolID and DestinationToolID
 for connection in connections_list:
     origin_tool_id = connection['OriginToolID']
     destination_tool_id = connection['DestinationToolID']
@@ -72,15 +75,18 @@ for connection in connections_list:
 # Get node positions from the graph
 node_positions = nx.get_node_attributes(G, 'pos')
 
-# Draw the graph with node positions
-plt.figure(figsize=(10, 6))
-nx.draw(G, pos=node_positions, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, font_weight='bold', arrows=True)
-labels = {(edge[0], edge[1]): f"{edge[0]}->{edge[1]}" for edge in G.edges()}
-nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=labels, font_color='red')
+# # Draw the graph with node positions
+# plt.figure(figsize=(10, 6))
+# nx.draw(G, pos=node_positions, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, font_weight='bold', arrows=True)
+# labels = {(edge[0], edge[1]): f"{edge[0]}->{edge[1]}" for edge in G.edges()}
+# nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=labels, font_color='red')
 
-# plt.title("DAG based on Workflow Nodes and Connections")
-# plt.show()
+# # plt.title("DAG based on Workflow Nodes and Connections")
+# # plt.show()
 
+# Get the topological order of the graph to represent an order in which the tools are connected to each other.
+# As "input" nodes have in-degree 0, these nodes are the very first items in the topological order as they are the very first
+# tools in a workflow.
 
 topological_order = list(nx.topological_sort(G))
 
@@ -89,12 +95,35 @@ print(topological_order)
 
 print("=" * 50)
 
+# Get nodes with in-degree zero (in other words, "input nodes")
+
+### Identifying such nodes helps to get the logic of the data transformations with Python.
+### As data transformation in Python can be chained on the same DataFrame, the goal is to:
+###         1. Identify "input" nodes
+###         2. Identify which nodes are connected to these "input" nodes and how they can be represented as subflows
+
+### The ultimate goals is to identify subflows in the workflow: a subflow is represented by a set of connected nodes ( or tools )
+### for which the transformations can be chained on the same DataFrame.
+### Ex: Input Data > Formula > Data Cleansing > Select is an example of subflows.
+
+in_degrees = dict(G.in_degree())
+
+# Find nodes with in-degree 0
+nodes_with_in_degree_zero = [node for node, in_degree in in_degrees.items() if in_degree == 0]
+
+# Output nodes with in-degree 0
+print("Nodes with in-degree 0:", nodes_with_in_degree_zero)
+
 # Create a new key-value pair "Order" for each element in nodes_list
 for node in nodes_list:
-    tool_id = node.get('Tool ID')
-    if tool_id in topological_order:
-        order = topological_order.index(tool_id)
-        node['Order'] = order
+    if node.get("Tool") != "ToolContainer":
+        tool_id = node.get('Tool ID')
+        if tool_id in topological_order:
+            order = topological_order.index(tool_id) + 1 # Initiate order at 1, containers will be assigned value 0
+            node['Order'] = order
+    else:
+        node['Order'] = 0
+
 
 nodes_list = sorted(nodes_list, key=lambda x: x['Order'])
 
@@ -110,7 +139,6 @@ with open(nodes_output_file_name, 'w') as output_file:
     dict_writer.writerows(nodes_list)
 
 # Output Connections info
-
 with open(connections_output_file_name, 'w') as output_file:
     dict_writer = csv.DictWriter(output_file, connections_keys)
     dict_writer.writeheader()
